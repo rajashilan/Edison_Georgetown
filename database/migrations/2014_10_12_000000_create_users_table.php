f3da930b501cf31b751d6513357e803e894bac08
Elyn Lim <elynlim120786@gmail.com>
2020-11-24 13:59:07 +0000

    2014_10_12_000000_create_users_table.php edited online with Bitbucket.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('user_id);
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('contact_number');
+            $table->string('staff_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('status');
             $table->rememberToken();
             $table->timestamps();
         });
