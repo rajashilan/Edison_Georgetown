d8b39a373822ce90b68e22116e2b7f3ae5c58cee
yininglyn <elynlim120786@gmail.com>
2021-01-29 00:34:21 +0800

    feedback store function (insert data) (have error)

--- app/Http/Controllers/FeedbackRatingController.php
+++ app/Http/Controllers/FeedbackRatingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Customer;
 use DB;
 
 class FeedbackRatingController extends Controller
@@ -17,4 +18,24 @@ class FeedbackRatingController extends Controller
       ]);
     }
 
+    public function store(Request $request){
+
+      $questions = DB::select('select f_q_id, question from feedback_questions');
+
+      $insert = DB::insert('insert into customer_feedbacks (customer_id, f_q_id, rating, remarks, status) values (?, ?, ?, ?, ?, ?)',
+      [$custID, $request->f_q_id, $request->rating, $request->remarks, 1]);
+
+      // $custID = DB::select('select customer_id from customers');// need to get customer_id
+      $custID = Customer::findOrFail($customer_id);
+      $fqID = $request->$questions->f_q_id;//need to get the question's id (f_q_id);
+      $rating = $request->$questions->f_q_id;
+      // $remarks = $request->$questions->f_q_id;
+      $insert->remarks = $request->input($questions->f_q_id);
+
+      $insert->save();
+
+      dd($insert);
+      return view('login-page');
+
+    }
 }
