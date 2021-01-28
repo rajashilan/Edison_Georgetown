252dd33485fa65eb4c3e761a40cd55a96020b30c
yininglyn <elynlim120786@gmail.com>
2021-01-28 17:10:16 +0800

    feedback controller create function code cleaning

--- app/Http/Controllers/FeedbackRatingController.php
+++ app/Http/Controllers/FeedbackRatingController.php
@@ -12,9 +12,6 @@ class FeedbackRatingController extends Controller
     public function create(Request $request){
       $questions = DB::select('select f_q_id, type, question from feedback_questions');
 
-      $custID = DB::select('select customer_id from customers');
-      dd($custID);
-
       return view('feedback-rating.form', [
         'questions' => $questions
       ]);
